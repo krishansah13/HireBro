6e8c2e7560e1340428bc4041099e9aa8b7ff2b89
Krishan <krishan.sah@geekyants.com>
2026-08-12 12:13:54 +0530

    feat: Created User model with schema for MongoDB using Mongoose

---
+++ src/lib/modals/Users.ts
@@ -0,0 +1,29 @@
+import {model, Schema} from "mongoose"
+const userSchema = new Schema({
+    id : {
+        type : String,
+    }, name : {
+        type : String,
+        required : true
+    }, email : {
+        type : String,
+        unique : true,
+        lowercase : true,
+        required : true
+    }, passwordHash : {
+        type : String,
+        required : true
+    }, role : {
+        type : String,
+        enum : ["seeker", "employer"],
+        required : true,
+        default : "seeker"
+    }, companyId : {
+        type : Schema.Types.ObjectId,
+        default : null,
+        ref : "Company"
+    }
+})
+
+const User = model("User", userSchema);
+export default User;
