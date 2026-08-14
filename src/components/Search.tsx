5129c610621acd6dcabff8eb1c9c18b9f2c62f4f
Krishan <krishan.sah@geekyants.com>
2026-08-14 11:59:57 +0530

    refactor: Header and SearchBar components for enhanced UI and functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import { Search } from "lucide-react"
 
 export default function SearchBar() {
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center z-10">
             <div className="m-2 w-3/4 bg-white rounded-full px-6 py-2 flex justify-between">
                 <Search className="m-2  text-gray-500" />
                 <input type="text" placeholder="Job Title or keyword" className="px-6 py-2 focus:border-0 w-[80%] text-black placeholder:text-gray-400 focus:ring-0 focus:outline-none" onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -11,7 +11,7 @@ export default function SearchBar() {
                         console.log("Search", e.currentTarget.value)
                     }
                 }} />
-                <button type="submit" className="bg-gray-600 px-6 py-2 rounded-full ">Search</button>
+                <button type="submit" className="bg-linear-to-br from-[#000000] via-[#0e0e6e] to-[#000000] px-6 py-2 rounded-full font-bold">Search</button>
             </div>
         </div>
     )
