8603e997ceb926c77c2c3909ae984e636b7709eb
Krishan <krishan.sah@geekyants.com>
2026-08-18 14:19:14 +0530

    feat: Update route matcher configuration for authentication to include base paths for dashboard and employer

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,8 +4,5 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-    matcher: [
-        "/dashboard/:path*",
-        "/employer/:path*",
-    ],
+  matcher: ["/dashboard", "/dashboard/:path*", "/employer", "/employer/:path*"],
 };
