401ea37ed8c20e1f5207929ec3dd9058cc7e58b9
Krishan <krishan.sah@geekyants.com>
2026-08-20 14:52:46 +0530

    feat: Add getJobApplicants function to retrieve job applications for a specific job

--- src/lib/employer-query.ts
+++ src/lib/employer-query.ts
@@ -1,3 +1,5 @@
+import User from "./models/User";
+import Application from "./models/Application";
 import Job from "./models/Job";
 import { connectToDatabase } from "./utils/db";
 import { serialize } from "./utils/serialize";
@@ -18,4 +20,24 @@ export async function getCompanyJobById(companyId: string, jobId: string) {
     
     const job = await Job.findOne({ _id: idParsed.data, companyId }).lean();
     return serialize(job);
+}
+
+const USER_SELECT="name email";
+export async function getJobApplicants(companyId: string, jobId : string) {
+    const job = await getCompanyJobById(companyId, jobId);
+    if(!job) {
+        return null;
+    }
+
+    await connectToDatabase();
+
+    void User;
+
+    const applications = await Application.find({jobId: job._id})
+        .populate({path:"userId", select: USER_SELECT})
+        .sort({appliedAt : -1})
+        .lean()
+    return {
+        job, applications: serialize(applications)
+    }
 }
